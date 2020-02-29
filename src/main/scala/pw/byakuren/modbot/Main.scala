cda6183725df1e89d968b56eedf830211b71a440
Brod8362 <brodriguez410@gmail.com>
2020-02-28 19:21:39 -0600

    Check command permissions before executing on new thread

--- src/main/scala/pw/byakuren/modbot/Main.scala
+++ src/main/scala/pw/byakuren/modbot/Main.scala
@@ -2,11 +2,12 @@ package pw.byakuren.modbot
 
 import java.io.File
 
-import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.{Member, Message, User}
 import net.dv8tion.jda.api.events.ReadyEvent
-import net.dv8tion.jda.api.{JDA, JDABuilder}
+import net.dv8tion.jda.api.{JDA, JDABuilder, Permission}
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import pw.byakuren.modbot.commands.CommandPermission.CommandPermission
 import pw.byakuren.modbot.commands.{Command, CommandPermission, ConversationList, SetLogChannel, ViewChatQueue}
 import pw.byakuren.modbot.config.BotConfig
 import pw.byakuren.modbot.conversation.ConversationTracker
@@ -19,6 +20,7 @@ object Main extends ListenerAdapter {
   implicit val sql: SQLConnection = new SQLConnection
   val config = new BotConfig(new File("config"))
   val prefix: String = config.getString("prefix").get
+  var ownerOption: Option[User] = None
 
   implicit val guildDataManager: GuildDataManager = new GuildDataManager
   implicit val conversationTracker: ConversationTracker = new ConversationTracker
@@ -49,6 +51,7 @@ object Main extends ListenerAdapter {
       case None =>
         throw new RuntimeException("Token not found, check config file")
     }
+    ownerOption = Some(jda.retrieveApplicationInfo().complete().getOwner)
   }
 
   override def onGuildMessageReceived(event: GuildMessageReceivedEvent): Unit = {
@@ -58,11 +61,25 @@ object Main extends ListenerAdapter {
       commandRegistry(args.head) match {
         case Some(command) =>
           //TODO check permission
-          command.run(event.getMessage, args drop 1)
+          if (permissionLevel(event.getMember) >= command.permissionLevel)
+            new Thread(() => command.run(event.getMessage, args drop 1)).start()
+          else
+            event.getMessage.reply("Insufficient permission")
         case _ =>
           event.getMessage.reply("Command not found")
       }
     }
+  }
+
+  def permissionLevel(member: Member): CommandPermission = {
+    for (owner <- ownerOption) {
+      if (member.getIdLong==owner.getIdLong) return CommandPermission.Debug
+    }
+    for (role <- guildDataManager(member.getGuild).moderatorRole) {
+      if (member.getRoles.contains(role)) return CommandPermission.Admins
+    }
+    if (member.hasPermission(Permission.ADMINISTRATOR)) return CommandPermission.Admins
+    CommandPermission.Everybody
   }
 
   override def onReady(event: ReadyEvent): Unit = {
@@ -74,7 +91,7 @@ object Main extends ListenerAdapter {
       try {
         writable.write(sql)
       } catch {
-        case e:Exception => e.printStackTrace()
+        case e: Exception => e.printStackTrace()
       }
     }
     jda.shutdown()
--- src/main/scala/pw/byakuren/modbot/commands/CommandPermission.scala
+++ src/main/scala/pw/byakuren/modbot/commands/CommandPermission.scala
@@ -2,5 +2,5 @@ package pw.byakuren.modbot.commands
 
 case object CommandPermission extends Enumeration {
   type CommandPermission = Value
-  val Everybody, Private, Admins, Debug = Value
+  val Everybody, Admins, Debug = Value
 }
